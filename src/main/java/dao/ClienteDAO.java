e0b84a594fcfb6f2243bd5b46126e5109ff6e03e
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-04-29 07:12:45 -0500

    ajustes

--- src/main/java/dao/ClienteDAO.java
+++ src/main/java/dao/ClienteDAO.java
@@ -1,12 +1,10 @@
 package dao;
 
-import java.util.List;
-
 import entity.Cliente;
 
 public interface ClienteDAO {
 
 	public abstract int insertaCliente(Cliente obj);
-	public abstract List<Cliente> listaClientePorNombre(String filtro);
+
 	
 }
--- src/main/java/dao/impl/MySqlClienteDAO.java
+++ src/main/java/dao/impl/MySqlClienteDAO.java
@@ -2,13 +2,9 @@ package dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
 import dao.ClienteDAO;
-import entity.Categoria;
 import entity.Cliente;
 import util.MySqlDBConexion;
 
@@ -46,49 +42,6 @@ public class MySqlClienteDAO implements ClienteDAO{
 		return salida;
 	}
 
-	@Override
-	public List<Cliente> listaClientePorNombre(String filtro) {
-		List<Cliente> lista = new ArrayList<Cliente>();
-		Connection conn = null;
-		PreparedStatement pstm = null;
-		ResultSet rs = null;
-		try {
-			conn = MySqlDBConexion.getConexion();
-			
-			String sql = "select c.*, a.nombre from cliente c inner join categoria a "
-					+ " on c.idcategoria =  a.idcategoria where c.nombre like ?";
-			pstm = conn.prepareStatement(sql);
-			pstm.setString(1, filtro);
-			log.info(">>>> " + pstm);
-
-			rs = pstm.executeQuery();
-			Cliente objCliente = null;
-			Categoria objCategoria = null;
-			while(rs.next()) {
-				objCliente = new Cliente();
-				objCliente.setIdCliente(rs.getInt(1));
-				objCliente.setNombre(rs.getString(2));
-				objCliente.setDni(rs.getString(3));
-				
-				objCategoria = new Categoria();
-				objCategoria.setIdCategoria(rs.getInt(4));
-				objCategoria.setNombre(rs.getString(5));
-				
-				objCliente.setCategoria(objCategoria);
-				
-				lista.add(objCliente);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pstm != null) pstm.close();
-				if (conn != null) conn.close();
-			} catch (Exception e2) {}
-		}
-		
-		return lista;
-	}
 	
 }
 
